84ae65eade8db78237a0320b8848c9d06d837700
quiringiuffre373 <quiringiuffre373@gmail.com>
2025-03-13 10:26:59 +0000

    Add 8.ts

---
+++ src/8.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mathematics-calculator',
+  templateUrl: './mathematics-calculator.component.html',
+  styleUrls: ['./mathematics-calculator.component.css']
+})
+export class MathematicsCalculatorComponent {
+  
+}
