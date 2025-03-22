0e54e8fe2cfc116c2f836f602aa7faaf65437912
quiringiuffre373 <quiringiuffre373@gmail.com>
2025-03-22 21:06:10 +0000

    Add 17.ts

---
+++ src/17.ts
@@ -0,0 +1,16 @@
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+function subtract(a: number, b: number): number {
+    return a - b;
+}
+
+function multiply(a: number, b: number): number {
+    return a * b;
+}
+
+function divide(a: number, b: number): number {
+    if (b === 0) throw new Error("Division by zero is not allowed!");
+    return a / b;
+}
