b98693620d1cc5b9d0c3d8e6ec24d8ce8afdd6f4
quiringiuffre373 <quiringiuffre373@gmail.com>
2025-04-07 03:05:30 +0000

    Add 27.ts

---
+++ src/27.ts
@@ -0,0 +1,11 @@
+import math
+
+def calculate(a, b, operation):
+    if operation == "add":
+        return a + b
+    elif operation == "subtract":
+        return a - b
+    elif operation == "multiply":
+        return a * b
+    elif operation == "divide":
+        return a / b
