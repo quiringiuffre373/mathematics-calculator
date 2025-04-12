ab3de2dbdd9c5b220f7e3e1fee494b478534f4ef
quiringiuffre373 <quiringiuffre373@gmail.com>
2025-04-12 07:27:58 +0000

    Add 31.ts

---
+++ src/31.ts
@@ -0,0 +1,11 @@
+import math
+
+def calculate(a, b):
+    return a + b
+
+def square(x):
+    return x * x
+
+print("1. 加法:", calculate(2, 3))
+print("2. 减法:", calculate(4, 2))
+print("3. 乘法:", square(5))
