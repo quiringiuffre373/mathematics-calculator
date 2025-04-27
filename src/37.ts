c761da487b1fb90cbfa5ad30033439bfab80e85d
quiringiuffre373 <quiringiuffre373@gmail.com>
2025-04-27 13:06:56 +0000

    Add 37.ts

---
+++ src/37.ts
@@ -0,0 +1,11 @@
+from math import sqrt
+
+def calculate(a, b, operation):
+    if operation == '+':
+        return a + b
+    elif operation == '-':
+        return a - b
+    elif operation == '*':
+        return a * b
+    elif operation == '/':
+        return a / b
